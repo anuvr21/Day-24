2a6dc50b74ae58b7d536a9d117c3e6a3b174c4e4
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-06 12:00:19 +0530

    Add files via upload

---
+++ EFDemo/EFDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Assignment2Entities())
+            {
+                /* var std = new Client_Master()
+                {
+                    ClientNo= "C00002",
+                    Name = "Bill",
+                    Address1 = "123 street",
+                    Address2="abc street",
+                    City="Kochi",
+                    State="Kerala",
+                    BalDue=40000,
+                    Phone_No=9876543210
+                }; */
+
+
+                var std = context.Client_Master.Where(s =>s.Name)<Client_Master>();
+                //std.Name = "Steve";
+
+                var std = context.Client_Master.First<Client_Master>();
+                context.Client_Master.Remove(std);
+
+                context.SaveChanges();
+
+                //context.Client_Master.Add(std);     --Insert
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
